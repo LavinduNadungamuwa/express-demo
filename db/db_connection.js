46d34059b52de9e5cda532714ad022464396d57d
LavinduNadungamuwa <lavindunadungamuwa@gmail.com>
2026-01-18 11:32:01 +0530

    db correction

--- db/db_connection.js
+++ db/db_connection.js
@@ -1,15 +1,19 @@
 const mysql = require('mysql2')
 
-let connection;
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'lav67#Sabra',
+    database: 'express_db',
+    port: 3307
+});
 
-function connectToDatabase() {
-    if (!connection) {
-        connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: 'lav67#Sabra',
-            database: 'express_db',
-            port: 3307
-        });
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database:', err);
+        return;
     }
-}
+    console.log('Connected to MySQL database successfully');
+});
+
+module.exports = connection;
